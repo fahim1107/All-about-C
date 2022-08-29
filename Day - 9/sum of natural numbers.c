4842326048dadab4ffa1b88d9ea356bb379cb8b8
fahim1107 <saiyanfaisal@gmail.com>
2022-08-30 00:08:31 +0600

    day 9

---
+++ Day - 9/sum of natural numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main ()
+{
+    int i,sum;
+
+    for (i=1;i<=10;i++)
+{
+
+
+        printf("%d\n",i);
+}
+
+
+        sum+=1
+
+        printf("The sum is : %d", sum);
+
+
+
+
+
+
+
+}
